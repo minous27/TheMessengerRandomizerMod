3f68855fa89362dd2e35f166c8ed12ae0547a5c7
minous27 <dill2726@hotmail.com>
2020-03-31 18:57:52 -0500

    True initial commit and added tests

---
+++ MessengerRando.Tests/RandomizerGenerationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessengerRando.Tests
+{
+    [TestClass]
+    public class RandomizerGenerationTests
+    {
+        [TestMethod]
+        public void TestItemRandomizerUtilLoad()
+        {
+            //Initilize the util class.
+            ItemRandomizerUtil.Load();
+            //Attempt to get a mapping
+            Dictionary<EItems, EItems> mapping = ItemRandomizerUtil.GenerateRandomizedMappings();
+            //Currently this test just makes sure that the Generate makes it all the way through the process and gives us back a mapping.
+            Assert.IsNotNull(mapping);
+        }
+    }
+}
